55961dda7d53d248146a21df99e4bf00c97dd3df
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2023-11-22 08:46:50 -0300

    settings auth and prisma
    npx prisma init
    npx prisma migrate dev
    
    npx prisma studio
    
    docker start mysql
    
    Install Docker
    https://efficient-sloth-d85.notion.site/Instalando-o-Docker-6290d9994b0b4555a153576a1d97bee2

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import React from 'react';
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import { getCssText } from '../../stitches.config';
 
-import { getCssText } from '../../stitches.config'
-
-export default function Document() {
-  return (
-    <Html lang="pt-BR">
-      <Head>
-      <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class Document extends NextDocument {
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head>
+          <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
