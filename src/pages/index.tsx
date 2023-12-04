8365b2764ba93c864a4dca8ae7831bf34610d344
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2023-12-04 15:23:43 -0300

    new pages and new components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,20 @@
-import { signOut, useSession } from "next-auth/react"
-import { NextPageWithLayout } from "./_app"
 import { DefaultLayout } from "@/layouts/DefaultLayout"
+import { ReactElement } from "react"
+import { NextPageWithLayout } from "./_app"
+import { HomeContainer } from "@/styles/pages/home"
+import { LatestRatings } from "@/components/LatestRatings"
+import { PopularBooks } from "@/components/PopularBooks"
 
 const HomePage: NextPageWithLayout = () => {
-  const { data } = useSession()
   return (
-    <>
-      {JSON.stringify(data, null, 2)}
-      <button onClick={() => signOut()}> deslogar</button>
-    </>
+    <HomeContainer>
+      <LatestRatings />
+      <PopularBooks />
+    </HomeContainer>
   )
 }
 
-HomePage.getLayout = (page) => {
+HomePage.getLayout = (page: ReactElement) => {
   return (
     <DefaultLayout title="Início">
       {page}
